905f57dca4d0a177fe65179aec2352ea534c44a9
DADOLLE Robin <roda@ouest.sesame.infotel.com>
2017-11-14 16:38:37 +0100

    end day

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
+
 
         ];
 
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,10 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
-
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class DefaultController extends Controller
 {
     /**
@@ -16,18 +15,80 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        echo "\n \033[0;31m Le choix saisie n'est pas valide \033[0m\n";
-        return $request;
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
 
     /**
-     * @Route("/name", name="homepage")
+     * @Route("/test", name="test")
+     * @Method({"GET"})
      */
-    public function nameAction(Request $request)
-    { 
-        $name =  array("test1","test2","test3");
-
-        return new JsonResponse(array('name' => $name));
-
+    public function getPlacesAction(Request $request)
+    {
+        return new JsonResponse(['status' => 200]);
     }
+
+    /**
+     * @Route("/name", name="name")
+     * @Method({"GET"})
+     */
+    public function getNamesAction(Request $request)
+    {
+        $name = new \stdClass();
+        $name->name = 'blabla';
+        $name->mail = '@test';
+
+        $test = array(
+            //'names' => array(
+                'name' => array(
+                    'test',
+                    'test1',
+                    'test2',
+                    'test3',
+                    ),
+                'test' => array(
+                    'test0',
+                    'test10',
+                    'test20',
+                    'test30',
+                    ),
+              //  )
+            );
+
+        return new JsonResponse($test);
+    }
+
+    /**
+     * @Route("/calendar", name="calendar")
+     * @Method({"GET"})
+     */
+    public function getCalendarAction(Request $request)
+    {
+        $jour = date("j");
+        $mois = date("n");
+        $year = date("Y");
+        $sem = date("N");
+
+        $calendar = array(
+            'day'   => $jour,
+            'month' => $mois,
+            'year'  => $year,
+            'sem'   => $sem
+            );
+
+        return new JsonResponse($calendar);
+    }
+
+    /**
+     * @Route("/test", name="testpost")
+     * @Method({"POST"})
+     *
+     */
+    public function postPlacesAction(Request $request)
+    {
+        return new JsonResponse(['status' => 201]);
+    }
+
 }
